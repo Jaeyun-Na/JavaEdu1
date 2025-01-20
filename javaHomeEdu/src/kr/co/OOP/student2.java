44b245e556039711abf2df7b6d907d2339a6049c
gejon <gejon@DESKTOP-1ILBTPR>
2025-01-20 16:58:32 +0900

    25.01.20

---
+++ javaHomeEdu/src/kr/co/OOP/student2.java
@@ -0,0 +1,11 @@
+package kr.co.OOP;
+
+public class student2 {
+
+	int studentId;
+	String studentName;
+	int koreanScore;
+	int mathScore;
+	String koreanSubject;		//과목 이름 변수 추가
+	String mathSubject;			//과목 이름 변수 추가
+}
