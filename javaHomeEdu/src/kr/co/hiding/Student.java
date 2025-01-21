3d7685c0c0ed4975aedf233bc2b0059b730ff9d5
gejon <gejon@DESKTOP-1ILBTPR>
2025-01-21 17:48:51 +0900

    25.1.21

---
+++ javaHomeEdu/src/kr/co/hiding/Student.java
@@ -0,0 +1,18 @@
+package kr.co.hiding;
+
+public class Student {
+
+	int studentId;
+	public String studentName;
+	int grade;
+	String address;
+
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	
+}
