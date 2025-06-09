88e7690154a34fa4de739be926374d55b2b5520f
gejon <gejon@118.36.10.27>
2025-06-09 17:45:54 +0900

    25.6.9

--- javaHomeEdu/src/polymorlphism/Customer.java
+++ javaHomeEdu/src/polymorlphism/Customer.java
@@ -14,10 +14,10 @@ public class Customer {
 	public Customer(int customerID, String customerName) {
 		this.customerID = customerID;
 		this.customerName = customerName;
-		initCustomer();
+		initCustomer();											//고객 등급과 보너스 포인트 적립률 지정 할수
 	}
 	
-	private void initCustomer() {
+	private void initCustomer() {								//생성자에서만 호출하는 메서드이므로 private로 선언, 멤버 변수의 초기화
 		customerGrade = "SILVER";
 		bonusRatio = 0.01;
 	}
@@ -30,4 +30,38 @@ public class Customer {
 	public String showCustomerInfo() {
 		return customerName + "님의 등급은 " + customerGrade + "이며, 보너스 포인트는 " + bonusPoint + "입니다.";
 	}
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public int getBonusPoint() {
+		return bonusPoint;
+	}
+
+	public void setBonusPoint(int bonusPoint) {
+		this.bonusPoint = bonusPoint;
+	}
+
+	public String getCustomerGrade() {
+		return customerGrade;
+	}
+
+	public void setCustomerGrade(String customerGrade) {
+		this.customerGrade = customerGrade;
+	}
+	
+	
 }
